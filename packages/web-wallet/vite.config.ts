248d8dfe6b6d8dfc8644ac43e76f90bfb28d5081
Apple <Apple@MacBook-Pro-2.local>
2025-03-18 15:20:08 +0500

    fix: setup changes after review

--- packages/web-wallet/vite.config.ts
+++ packages/web-wallet/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
             less: {
                 javascriptEnabled: true,
             },
+        },
+    },
+    resolve: {
+        alias: {
+            '@shared-ui': resolve(__dirname, '../shared-ui/src'),
         },
     },
 })
