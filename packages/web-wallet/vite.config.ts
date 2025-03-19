5d3f0d007856c9e627fb548d098020363defdbd4
Apple <Apple@MacBook-Pro-2.local>
2025-03-19 12:39:46 +0500

    feat: shared-ui style import changes

--- packages/web-wallet/vite.config.ts
+++ packages/web-wallet/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +13,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@shared-ui': resolve(__dirname, '../shared-ui/src'),
+            '@style': 'shared-ui/styles',
         },
     },
 })
